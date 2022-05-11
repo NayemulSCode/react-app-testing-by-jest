70cffaa57f24f426ed283696a0749fdc6189ded2
NayemulSCode <nayemulhasan2016@gmail.com>
2022-05-11 16:22:04 +0600

    test link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Link from './components/LinkTest/Link';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        <h1>React app test with JEST</h1>
+       <Link />
       </header>
     </div>
   );
